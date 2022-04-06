360fada0e3e4e0576557853c6de7c79f1039dc39
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-05 21:47:29 -0500

    Originacion

---
+++ src/main/java/org/toledano/tareas/TareasSpringApplication.java
@@ -0,0 +1,13 @@
+package org.toledano.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/toledano/tareas/TareasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toledano.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
